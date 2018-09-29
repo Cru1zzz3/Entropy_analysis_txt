4b89522789599142edc6c8cffd8ca7efdb55ac77
Stanislav Udartsev <udartzev_sv@mail.ru>
2018-09-30 02:33:44 +0300

    Add comparasion Hartley and Shennon formula

--- Source.cpp
+++ Source.cpp
@@ -6,46 +6,41 @@
 
 using namespace std;
 
+double log2( double n )  
+{  
+    // log(n)/log(2) is log2.  
+    return log( n ) / log( 2 );  
+}  
+
 int main(void){
 	setlocale(LC_ALL, "rus");
-	ifstream inputFile("input.txt"); // открыл файл для чтения
-	string line;
-	string::iterator lineIt;
+	fstream inputFile("inputEnglish.txt"); // открыл файл для чтения
 	char ch;
 	double sumSymbols = 0;
 	int sumAlphabet = 0;
-	double probability;
+	double probability = 0;
+	double entropy = 0;
+	double hartley = 0;
 	multimap<char,int> alphabet;
 	multimap<char,int>::iterator alphabetIter;
 	cout.precision(10);
-	
+
 	if (!inputFile.is_open()){
 		cout << "File could't be open..." << endl;
 	}
 	else {
-		while (getline(inputFile,line)){
-			if (line == ""){
-			//	cout << endl;
-			}
-			else {
-				//cout << line << endl;
-				for (lineIt = line.begin(); lineIt != line.end(); ++lineIt){ // посимвольное считывание из строки
-					//cout << *lineIt;
-					ch = *lineIt; // символ из итератора
+		while (inputFile){
+					inputFile >> ch;
+					//cout << ch;
 					alphabetIter = alphabet.find(ch);
-					if (alphabetIter != alphabet.end()){ // если нашёл в алфавите
-						//cout << " Found in alphabet" << endl;
+					if (alphabetIter != alphabet.end() ){ // если нашёл в алфавите
 						(alphabetIter->second)++;
-						sumSymbols = alphabetIter->second + sumSymbols;
+						sumSymbols++;
 					}
 					else {
-						//cout << " Not found " << endl; // не нашёл в алфавите
 						alphabet.insert (pair<char,int>(ch,1));
-					}
-
-					
-				}
-			}
+						sumSymbols++;
+					}		
 		}
 	}
 
@@ -55,10 +50,17 @@ int main(void){
 		cout << alphabetIter->first << "\t" << alphabetIter->second;
 		probability = (alphabetIter->second)/sumSymbols;
 		cout << fixed <<  "\t" << probability  << endl;
+		entropy = (probability * log(probability)) + entropy;
 	}
+
 	
 	cout << "\n\nSymbols in alphabet: " << alphabet.size() << endl;
-	cout << "Sum of Symbols: " << sumSymbols << endl;
+	cout.precision(0);
+	cout  << "Sum of Symbols: " << sumSymbols << endl;
+	hartley  = log2(alphabet.size());
+	cout << "Hartley formula log2N: " << hartley << endl;
+	cout.precision(4);
+	cout << "Entropy of file: " << -entropy << endl;
 
 	return 0;
 }
